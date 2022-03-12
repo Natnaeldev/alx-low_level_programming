21e8f5addf716fb91ad38c37928c6a94d716708a
Natnaeldev <nberhane44@gmail.com>
2022-03-11 22:23:17 -0800

    Task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int N;
+for (N = 48; N <= 57; N++)
+{
+putchart(N);
+ if(N!=57)
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
+}
