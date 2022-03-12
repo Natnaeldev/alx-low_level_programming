1e25be47c47d0e57eb4a28bc18e7784b83ea2af7
Natnaeldev <nberhane44@gmail.com>
2022-03-11 18:18:40 -0800

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char N;
+for (L = 'a'; N <= 'z'; N++)
+{
+putchar(N);
+}
+ for (N = 'A'; N <= 'Z'; N++)
+{
+putchar(N);
+}
+putchar('\n');
+return (0);
+}
